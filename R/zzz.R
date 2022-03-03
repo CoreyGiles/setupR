970516063356047a46a2685fd52ec827f4be094c
unknown <corey.giles@baker.edu.au>
2022-03-03 13:16:49 +1100

    fas

--- R/zzz.R
+++ R/zzz.R
@@ -4,6 +4,6 @@
   packageStartupMessage(
     paste0("If you loaded this package, you should be a member of the Metabolomics Lab at the Baker Institute.\n",
            "If you need access to the Q drive library, run: fixLibPath()\n",
-           # "If you need to install some critical libraries, run: downloadLibs()\n",
+           "If you need to install some critical libraries, run: downloadLibs()\n",
            "Apart from that, have a great day :)"))
 }
